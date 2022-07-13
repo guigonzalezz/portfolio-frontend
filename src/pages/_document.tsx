a6dec7de132ff681eb9107a257644c130817d684
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-12 21:19:00 -0300

    chore: added mui into the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,58 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect } from 'react';
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
 
-export default function Document ()  {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Guilherme Gonzalez</title>
-        <meta name="description" content="Guilherme Gonzalez Portfolio" />
-        <link rel="icon" href="/logo_fundo.ico"/>
-        
-        <link href="http://fonts.cdnfonts.com/css/signpainter" rel="stylesheet"></link>
-        <link href="http://fonts.cdnfonts.com/css/roboto-mono" rel="stylesheet"></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import createEmotionCache from '../utility/createEmotionCache';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/logo_fundo.ico"/>
+          <link href="http://fonts.cdnfonts.com/css/signpainter" rel="stylesheet"/>
+          <link href="http://fonts.cdnfonts.com/css/roboto-mono" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  /* eslint-disable */
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) => (props) =>
+        <App emotionCache={cache} {...props} />,
+    });
+  /* eslint-enable */
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
