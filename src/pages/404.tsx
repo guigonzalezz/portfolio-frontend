0312e472ea96fbf93633d9732bcb6036e2df43d7
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-24 19:10:40 -0300

    feat: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Router from "next/router";
+
+import styles from '../styles/404.module.scss'
+
 const NotFound = () => (
   <>
     <Head>
@@ -6,7 +11,12 @@ const NotFound = () => (
         404 | Not found
       </title>
     </Head>
-    <p>Not found!</p>
+    <div className={styles.main}>
+      <Image width={100} height={100} src="/logo.png" alt="Guilherme Gonzalez Logo"/>
+      <span className={styles.number}>404</span>
+      <span className={styles.phrase}>Ops, page not found!</span>
+      <span onClick={() => Router.push("/")} className={styles.redirect}>&lt; Lets go back home &gt;</span>
+    </div>
   </>
 );
 
