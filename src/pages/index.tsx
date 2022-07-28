4c4a51eb7175cb1f40a1f5f065d768dc87090c6a
guigonzalezz <gcgonzalez99@hotmail.com>
2022-07-28 20:18:39 -0300

    feat: added the new mytabs with dynamic data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,36 @@
+import { Box } from '@mui/material'
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import MyTabs from '../components/Tabs'
 
 
 import styles from '../styles/Home.module.scss'
+import { getGithubGistData, getGithubReposData } from '../utility/githubData'
 import { getGraphQLData } from '../utility/graphqlData'
 
+interface IProps {
+  name: string;
+  data: any;
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
   return {
     props: {
-      data: await getGraphQLData()
+      data: [
+        ...await getGraphQLData(),
+        await getGithubReposData(),
+        await getGithubGistData()
+      ]
     }, 
     revalidate: 3600, //3600sec = 60min = 1h
   };
 }
 
-const Home: NextPage = ({data}:any) => {
+const Home: NextPage<{data: IProps[]}> = ({data}) => {
+  const firstSection = data.filter((item: IProps) => ['about', 'experience', 'education', 'skill'].includes(item.name))
+  const secondSection = data.filter((item: IProps) => ['project', 'repositories', 'gist'].includes(item.name))
 
   return (
     <>
@@ -31,14 +44,10 @@ const Home: NextPage = ({data}:any) => {
           <p>
             Photos
           </p>
-
-          <p>
-            Aboutme/Experience/Education/Skills
-          </p>
-
-          <p>
-            Projects/Repositories/Gist
-          </p>
+          
+          <MyTabs data={firstSection}/>
+         
+          <MyTabs data={secondSection}/>
 
           <p>
             Contact
