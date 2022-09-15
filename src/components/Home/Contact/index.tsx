3c63e85a418861df135553472efac3134a43d84a
guigonzalezz <gcgonzalez99@hotmail.com>
2022-09-14 21:37:12 -0300

    style:  section started to be stylized

--- src/components/Home/Contact/index.tsx
+++ src/components/Home/Contact/index.tsx
@@ -1,15 +1,27 @@
-import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, Input, Radio, RadioGroup, TextField } from '@mui/material';
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
 import Image from 'next/image';
 import * as React from 'react';
 import styles from './Contact.module.scss'
-export default function Contact({ backgroundImg, contactImage}: any) {
+import { useForm } from 'react-hook-form';
+import { Input } from '../../Input';
+import { Select } from '../../Select';
+import IFormValues from '../../../shared/interfaces/formValues.interface';
+import { InputRadio } from '../../Input Radio';
+import { InputMultiline } from '../../Input Multiline';
 
- 
+
+export default function Contact({ backgroundImg, contactImage }: any) {
+  const { register, handleSubmit, formState: { errors }, } = useForm<IFormValues>();
+
+  const onSubmit = (data: any) => {
+    console.log(JSON.stringify(data));
+  };
+  // Continue using this site as a reference -> https://react-hook-form.com/get-started/
   return (
     <Box id="contact" className={styles.box}>
       <Box>
         <Box>
-          <Image src={backgroundImg} alt="background image" layout='fill'/>
+          <Image src={backgroundImg} alt="background image" layout='fill' className={styles.backgroundImage}/>
         </Box>
         <section className={styles.mainSection}>
           <Box className={styles.leftSection}>
@@ -17,25 +29,26 @@ export default function Contact({ backgroundImg, contactImage}: any) {
             <p>Let&apos;s discuss whenever your ideia is, and let&apos;s try together to inovate or create a brand new product.</p>
             <p>Depeding on the project size we can choose the best value to make this happen.</p>
             <p>Let&apos;s work together :)</p>
-            <img src={contactImage.url} alt="person in a desk"/>
+            <Image src={contactImage.url} alt="person in a desk" width={contactImage.width} height={contactImage.height} />
           </Box>
           <Box className={styles.rightSection}>
-              <label className={styles.labelInput} htmlFor="name-input">Name</label>
-              <input type="text" id="name-input" name="name-input" className={styles.inputField}/>
+            <form onSubmit={handleSubmit(data => onSubmit(data))}> 
 
-              <p>Which way do you want to get in touch?</p>
-              <input type="radio" id="contact-option-input-1" name="contact-option-input" value="email" className={styles.radioField}/>
-              <label htmlFor="contact-option-input-1">E-mail</label>
-              <input type="radio" id="contact-option-input-2" name="contact-option-input" value="phone" className={styles.radioField}/>
-              <label htmlFor="contact-option-input-2">Phone</label> 
-              
-              <label className={styles.labelInput} htmlFor="contact-input">Contact</label>
-              <input type="text" id="contact-input" name="contact-input" className={styles.inputField}/>
-              
-              <label className={styles.labelInput} htmlFor="message-input">Message</label>
-              <input type="text" id="message-input" name="message-input" className={styles.inputField}/>
+              <Input register={register} label="Name" {...register("Name")} required />
+              {errors.Name && <p className={styles.errorMessage} >Your name is required.</p>}
 
-              <Button variant="contained">Send Message</Button>
+              <InputRadio register={register} title="Which way do you want to get in touch?" required label="Option" {...register("Option")} radios={[{ value: 'email', text: 'E-mail' }, { value: 'phone', text: 'Phone' }]}/>
+              {errors.Option && <p className={styles.errorMessage}>Please select an option.</p>}
+
+              <Input register={register} label="Contact" {...register("Contact")} required />
+              {errors.Contact && <p className={styles.errorMessage}>Your contact is required.</p>}
+
+              <InputMultiline register={register} label="Message" {...register("Message")} required />
+              {errors.Message && <p className={styles.errorMessage}>Please write a message.</p>}
+
+              <input type="submit" className={styles.submitButton} />
+
+            </form>
           </Box>
         </section>
       </Box>
